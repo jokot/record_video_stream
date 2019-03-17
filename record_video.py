53932e906eba40b46d16747fceaea86349908611
joko triyanto <jokotriyanto123@outlook.com>
2019-03-17 10:51:43 +0700

    first commit

---
+++ record_video.py
@@ -0,0 +1,53 @@
+import numpy as np
+import cv2
+import time
+
+
+
+def record_video(url,file_name,duration):
+    # Create a VideoCapture object
+    cap = cv2.VideoCapture(url)
+
+    # Check if ther is internet /  the link is correct
+    if (cap.isOpened() == False):
+        print("No internet conection or the link is broken")
+
+    # Default resolutions of the frame are obtained.The default resolutions are system dependent.
+    # We convert the resolutions from float to integer.
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+
+    # Define the codec and create VideoWriter object.The output is stored in 'file_name' file.
+    out = cv2.VideoWriter(file_name,cv2.VideoWriter_fourcc('M','J','P','G'),10,(frame_width,frame_height))
+
+    start_time = time.time()
+    while time.time() - start_time < duration:
+        ret, frame = cap.read()
+
+        if ret == True:
+            # Write the frame into the file 'file_name'
+            out.write(frame)
+            
+            # Display the resulting frame 
+            cv2.imshow('recording....', frame)
+
+            # Press Q on keyboard to stop recording
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        # Break the loop
+        else:
+            break
+        
+    # When everything done, release the video capture and video write objects
+    cap.release()
+    out.release()
+
+####------> Main Program <------####------> Main Program <------####------> Main Program <------####------> Main Program <------####------> Main Program <------####
+record_video('http://cctv-dishub.sukoharjokab.go.id/zm/cgi-bin/nph-zms?mode=jpeg&monitor=8&scale=100&maxfps=15&buffer=1000&user=user&pass=user',
+             'output.avi',
+             120)
+
+
+# Closes all the frames
+cv2.destroyAllWindows() 
